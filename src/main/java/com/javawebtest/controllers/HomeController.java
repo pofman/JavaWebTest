cdaf19b1ed25b4b55d0ddebabdd10483a9edd6c8
Martin Mendez <martin.mendez@globant.com>
2015-08-23 22:11:54 -0300

    more advances

--- src/main/java/com/javawebtest/controllers/HomeController.java
+++ src/main/java/com/javawebtest/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.javawebtest.controllers;
 
+import com.javawebtest.daos.ITeamDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
+
+    private ITeamDAO teamDAO;
+
+    @Autowired(required=true)
+    public void setTeamDao(ITeamDAO teamDao){
+        this.teamDAO = teamDao;
+    }
+
+
     @RequestMapping(value="/", method = RequestMethod.GET)
     public String printWelcome(ModelMap model) {
         model.addAttribute("message", "Hello world");
